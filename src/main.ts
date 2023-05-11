d1634d8fb3cc04a4d8a3c26a1ef4590850d21f75
Mr_PPC <ppcandtk@gmail.com>
2023-05-11 11:32:47 +0800

    feat 配置侧边栏背景

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,16 @@ import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import router from './router/config'
 import App from './App.vue'
+import './style.css'
+import './style/element/style.scss'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+
 const pinia = createPinia()
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.use(ElementPlus, {
     locale: zhCn,
 })
--- src/pages/public/Layout/index.vue
+++ src/pages/public/Layout/index.vue
@@ -1,48 +1,93 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import asideBgImage from '../../../assets/images/aside/aside-bg.jpg'
 export default defineComponent({
+    setup() {
+       
+        let modeType = ref(false) //导航栏显示位置
+        let modeList = ['aside', 'header', 'main', 'footer']
+        let asiderData = reactive({//导航栏信息
+            activeTextColor: '#ffd04b',
+            bgColor: "rgba(0,0,0,0.2)",
+            textColor: '#FFF',
+            bgImage: asideBgImage
+        })
 
+        return {
+            modeList,
+            modeType,
+            asiderData,
+        }
+    }
 })
+
 </script>
 
 <template>
     <div class="common-layout">
         <el-container>
+            <el-header v-if="modeType" class="aside-box">
+                <el-menu mode="horizontal" :router="true" active-text-color="#ffd04b" background-color="rgba(0,0,0,0)"
+                    class="el-menu-vertical-demo" default-active="2" text-color="#fff">
+                    <el-sub-menu index="index">
+                        <template #title>
+                            <el-icon>
+                                <location />
+                            </el-icon>
+                            <span>Navigator One</span>
+                        </template>
+                        <el-menu-item-group title="Group One">
+                            <el-menu-item index="home">home</el-menu-item>
+                            <el-menu-item index="test">test</el-menu-item>
+                        </el-menu-item-group>
+                    </el-sub-menu>
+                </el-menu>
+            </el-header>
+            <el-aside width="200px" v-if="!modeType" class="aside-box">
+                <el-menu :router="true" :active-text-color="asiderData.activeTextColor"
+                    :background-color="asiderData.bgColor" class="el-menu-vertical-demo" default-active="2" :text-color="asiderData.textColor">
+                    <el-sub-menu index="index">
+                        <template #title>
+                            <el-icon>
+                                <location />
+                            </el-icon>
+                            <span>Navigator One</span>
+                        </template>
+                        <el-menu-item-group title="Group One">
+                            <el-menu-item index="home">home</el-menu-item>
+                            <el-menu-item index="test">test</el-menu-item>
+                        </el-menu-item-group>
+                    </el-sub-menu>
+                </el-menu>
+            </el-aside>
             <el-container>
-                <el-aside width="200px">
-                    <el-menu :router="true" active-text-color="#ffd04b" background-color="#545c64"
-                        class="el-menu-vertical-demo" default-active="2" text-color="#fff" @open="handleOpen"
-                        @close="handleClose">
-                        <el-sub-menu index="index">
-                            <template #title>
-                                <el-icon>
-                                    <location />
-                                </el-icon>
-                                <span>Navigator One</span>
-                            </template>
-                            <el-menu-item-group title="Group One">
-                                <el-menu-item index="home">home</el-menu-item>
-                                <el-menu-item index="test">test</el-menu-item>
-                            </el-menu-item-group>
-                        </el-sub-menu>
-                    </el-menu>
-                </el-aside>
-                <el-container>
-                    <el-main>
-                        <router-view v-slot="{ Component }">
-                            <keep-alive>
-                                <transition name="fade">
-                                    <component :is="Component" />
-                                </transition>
-                            </keep-alive>
-                        </router-view>
-                    </el-main>
-                </el-container>
-
+                <el-header>Header</el-header>
+                <el-main>
+                    <router-view v-slot="{ Component }">
+                        <keep-alive>
+                            <transition name="fade">
+                                <component :is="Component" />
+                            </transition>
+                        </keep-alive>
+                    </router-view>
+                </el-main>
+                <el-footer>Footer</el-footer>
             </el-container>
 
         </el-container>
+
     </div>
 </template>
-<style scoped></style>
+<style scoped lang="scss">
+.common-layout {
+    width: 100%;
+    height: 100%;
+}
+
+.aside-box {
+    background: $default;
+    background: v-bind("'url('+asiderData.bgImage+')'");
+    background-position: center;
+    color: #000;
+}
+</style>
